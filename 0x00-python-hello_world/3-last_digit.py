423a0b1c9edfb875516edd9b35358697d8d31695
conkobar <connorkleine@gmail.com>
2022-08-30 10:46:45 -0500

    i think i did it this time

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -17,12 +17,14 @@ if number < 0:
 print(last, end='')
 
 # finer specifications
-if last > '5':
-        if number > 0:
-                print(" and is greater than 5")
-
-elif last == '0':
+if last == '0':
         print(" and is 0")
 
 else:
-        print(" and is less than 6 and not 0")
+        if number > 0:
+                if last > '5':
+                        print(" and is greater than 5")
+                else:
+                        print(" and is less than 6 and not 0")
+        else:
+                        print(" and is less than 6 and not 0")
