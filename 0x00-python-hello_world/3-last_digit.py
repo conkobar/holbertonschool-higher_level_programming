41ffe6b0149198b3df233cbbea7e61a37ca7672f
conkobar <connorkleine@gmail.com>
2022-08-30 10:30:03 -0500

    actually read the rest of the task this time

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+# stringify the number
 num_str = repr(number)
-print(num_str[-1])
+last = num_str[-1]
+
+# start printing script
+print("Last digit of", number, "is ", end='')
+
+# check if @number is negative
+if number < 0:
+        if last != 0:
+                print("-", end='')
+
+print(last, end='')
+
+# finer specifications
+if last > '5':
+        if number > 0:
+                print(" and is greater than 5")
+
+elif last == '0':
+        print(" and is 0")
+
+elif last < '6':
+        print(" and is less than 6 and not 0")
