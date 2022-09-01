90c6a67db75b12d845780f849b4a7ea326d553b7
conkobar <connorkleine@gmail.com>
2022-09-01 15:08:59 -0500

    task 6

---
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    # print matrix with given format
+    if not matrix:
+        return None
+
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            if y == len(matrix[x]) - 1:
+                print("{:d}".format(matrix[x][y]), end="")
+            else:
+                print("{:d}".format(matrix[x][y]), end=" ")
+
+        print()
