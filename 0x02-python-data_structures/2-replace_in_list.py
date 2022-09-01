f2d7ad77d12af342eb8e0a6ae679b25ee82fedef
conkobar <connorkleine@gmail.com>
2022-09-01 13:46:02 -0500

    idfk

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -6,4 +6,5 @@ def replace_in_list(my_list, idx, element):
     for x in range(len(my_list)):
         if x == idx:
             my_list[x] = element
-            return my_list
+
+    return my_list
