d7c004952c03076cc9cd0764784440bc8974304a
conkobar <connorkleine@gmail.com>
2022-09-01 13:39:02 -0500

    lets try again

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,8 @@ def replace_in_list(my_list, idx, element):
 
     tmp = my_list
 
-    if idx > 0 and idx < (len(my_list) - 1):
-        tmp[idx] = element
-        return tmp
+    if idx < 0 and idx >= len(my_list):
+        return None
+
+    tmp[idx] = element
+    return tmp
