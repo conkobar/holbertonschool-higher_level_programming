258be13a3f8a27a0b4075a00ad140f7ddd8859ae
conkobar <connorkleine@gmail.com>
2022-09-01 14:23:30 -0500

    task 3 strugs

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -4,8 +4,7 @@
 def print_reversed_list_integer(my_list=[]):
     # print given list in reverse
     my_list.reverse()
-    l_length = len(my_list)
 
-    if l_length > 0:
-        for i in range(l_length):
+    if my_list:
+        for i in range(len(my_list)):
             print("{:d}".format(my_list[i]), sep="\n")
