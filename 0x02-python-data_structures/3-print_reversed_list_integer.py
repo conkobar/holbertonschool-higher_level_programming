d3ce94fe912290128e37b2ecb1cfa327d20fd23c
conkobar <connorkleine@gmail.com>
2022-09-01 14:21:52 -0500

    task 3 edging

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,8 @@
 def print_reversed_list_integer(my_list=[]):
     # print given list in reverse
     my_list.reverse()
+    l_length = len(my_list)
 
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]), sep="\n")
+    if l_length > 0:
+        for i in range(l_length):
+            print("{:d}".format(my_list[i]), sep="\n")
