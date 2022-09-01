111ef776d6b2bcdf1f8ac4fc2fed4a7d045ff6b2
conkobar <connorkleine@gmail.com>
2022-09-01 14:19:42 -0500

    task 3

---
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    # print given list in reverse
+    my_list.reverse()
+
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]), sep="\n")
