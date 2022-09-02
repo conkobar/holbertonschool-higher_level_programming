0c096f6ebb1aec045c1ceec61a8b1a6542563149
conkobar <connorkleine@gmail.com>
2022-09-01 20:14:36 -0500

    revising 9 further

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -3,8 +3,8 @@
 
 def delete_at(my_list=[], idx=0):
     # deletes item at index
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         del(my_list[idx])
 
     # return list if it exists
-    return my_list if my_list else None
+    return my_list
