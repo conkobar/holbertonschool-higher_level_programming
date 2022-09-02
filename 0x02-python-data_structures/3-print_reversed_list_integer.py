18d63aadf2da934b43c0733c92abe28e3a09bec0
conkobar <connorkleine@gmail.com>
2022-09-01 19:09:02 -0500

    returning to 3

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -3,8 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     # print given list in reverse
-    my_list.reverse()
-
     if my_list:
+        my_list.reverse()
         for i in range(len(my_list)):
             print("{:d}".format(my_list[i]), sep="\n")
