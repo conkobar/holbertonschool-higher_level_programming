6880cc9c4d7ad7b947f86cac568a6f388a004fb5
conkobar <connorkleine@gmail.com>
2022-09-01 20:02:01 -0500

    revising task 5 further

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -3,7 +3,4 @@
 
 def no_c(my_string):
     # replace c & C in given string
-    if my_string:
-        return my_string.translate({ord(c): None for c in 'cC'})
-    else:
-        return None
+    return my_string.translate({ord(c): None for c in 'cC'})
