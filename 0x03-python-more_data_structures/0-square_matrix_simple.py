a47b0b138f1bb1830c3c1d7379f491672d527ff9
conkobar <connorkleine@gmail.com>
2022-09-06 11:33:08 -0500

    task 0 again

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -3,19 +3,12 @@
 
 def square_matrix_simple(matrix=[]):
     # completes squares of all ints in matrix
-    # create copy of matrix
+    # create empty matrix
     dupe = [[]]
 
     # append squares to new matrix
     for idx in matrix:
-        temp = []
-        for i in idx:
-            temp.append(i ** 2)
-        dupe.append(temp)
-
-    # remove empty element
-    if dupe[1]:
-        del(dupe[0])
+        dupe.append(list(map(lambda x: x ** 2, i)))
 
     # return new matrix
     return dupe
