57a62da4bbca22d05570e443d66048f9ecf5f0f5
conkobar <connorkleine@gmail.com>
2022-09-08 11:00:58 -0500

    0x03 task 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 
+
 def roman_to_int(roman_string):
+    """ convert string of roman numerals to int """
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = 0
+
+    if type(roman_string) == str:
+        last = 0
+        for i in roman_string:
+            if last != 0 and last < values[i]:
+                sum -= last
+            else:
+                sum += last
+            last = values[i]
+        sum += last
+
+    return sum
