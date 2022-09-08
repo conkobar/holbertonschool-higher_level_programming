a9e171b6b5fc4d8703299f8aa00989bd28c5cb9b
conkobar <connorkleine@gmail.com>
2022-09-07 20:00:58 -0500

    task 1

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" expanding on existing square class """
+
+
+class Square:
+    """ further attributes given to the square """
+
+    def __init__(self, size=0):
+        """ size must be int and > 0 """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
