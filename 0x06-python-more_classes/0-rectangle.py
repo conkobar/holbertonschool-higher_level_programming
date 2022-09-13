88e96219e93966e77443e9b79682d272a20b02d2
conkobar <connorkleine@gmail.com>
2022-09-13 11:01:42 -0500

    more classes, task 0

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" make a rectangle out of given values """
+
+
+class Rectangle:
+    """ class to construct a rectangle """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be a ")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be a ")
+        self.__height = value
+
---
+++ 0x06-python-more_classes/README.md
@@ -0,0 +1,6 @@
+# [<center>0x06. Python - More Classes and Objects - 1152</center>](https://intranet.hbtn.io/projects/1152#quiz-completed)
+ ---
+ ### <center>Description</center> 
+ ##### &emsp; More classes and objects.
+ ---
+ [<center>Buzz Kleine</center>](github.com/conkobar)
