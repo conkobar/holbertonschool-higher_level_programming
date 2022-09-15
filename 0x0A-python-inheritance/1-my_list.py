a9d1b8c3dba7fac9c03eae490d39a1f787767a03
conkobar <connorkleine@gmail.com>
2022-09-15 14:09:07 -0500

    task one but more

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" new class inherited from list """
+"""new class inherited from list"""
 
 
 class MyList(list):
-    """ new list type object """
+    """new list type object"""
     def print_sorted(self):
         print(sorted(self))
