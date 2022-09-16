845636e1b6d007d476d84fb9bb44cd4e306cda9b
conkobar <connorkleine@gmail.com>
2022-09-15 22:01:55 -0500

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""empty class per given task"""
+
+
+class BaseGeometry:
+    """new class per given task"""
+    def area(self):
+        raise Exception("area() is not implemented")
