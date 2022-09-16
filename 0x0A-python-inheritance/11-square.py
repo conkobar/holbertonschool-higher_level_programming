971771485df3e713e7aa36cb1cc2cfa6934b3072
conkobar <connorkleine@gmail.com>
2022-09-15 23:19:33 -0500

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""empty class per given task"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """new square class forked from rectangle class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
