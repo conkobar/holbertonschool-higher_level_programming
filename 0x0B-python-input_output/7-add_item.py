680bc215b4e421f6250d8957625bddaef730ab76
conkobar <connorkleine@gmail.com>
2022-09-19 21:42:45 -0500

    omg

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 if __name__ == "__main__":
     import sys
     try:
-        jputs = load_from_json_file(add_item.json)
+        jputs = load_from_json_file("add_item.json")
     except:
         jputs = []
 
