5effbf769e4cff7bbe24c0b5978cb3cebc24e026
conkobar <connorkleine@gmail.com>
2022-09-19 21:36:13 -0500

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,10 @@
 """adds args to python list and save to file"""
 import sys
 import json
-save = __import__('5-save_to_json_file').save_to_json_file
-lode = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-jputs = lode(add_item.json)
+jputs = load_from_json_file(add_item.json)
 jputs += sys.argv[1:]
-save(jputs, "add_item.json")
+save_to_json_file(jputs, "add_item.json")
