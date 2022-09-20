a07938a81ff85ac5e4009dbeeb75f877b601dbcd
conkobar <connorkleine@gmail.com>
2022-09-19 21:42:24 -0500

    task 7 idkkkkkkk

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """adds args to python list and save to file"""
-import sys
-import json
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+if __name__ == "__main__":
+    import sys
+    try:
+        jputs = load_from_json_file(add_item.json)
+    except:
+        jputs = []
 
-try:
-    jputs = load_from_json_file(add_item.json)
-except:
-    jputs = []
-
-save_to_json_file(jputs + argv[1:], "add_item.json")
+    save_to_json_file(jputs + argv[1:], "add_item.json")
