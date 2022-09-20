c812b3f15367a0efbf590a8bbe4d9928d02f7c68
conkobar <connorkleine@gmail.com>
2022-09-19 21:40:28 -0500

    missing checks task 7 idk

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,10 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 try:
     jputs = load_from_json_file(add_item.json)
 except:
     jputs = []
 
-jputs += sys.argv[1:]
-save_to_json_file(jputs, "add_item.json")
+save_to_json_file(jputs + argv[1:], "add_item.json")
