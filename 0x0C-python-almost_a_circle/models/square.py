010069c3ed3e2b42421dd9b4560a9318f2cc1cff
conkobar <connorkleine@gmail.com>
2022-09-20 14:23:31 -0500

    task 1`0 missing a check

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,10 @@ class Square(Rectangle):
     """square class inheriting from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string & print functionality"""
+        return "[{}] ({}) {}/{} - {}".format(
+            type(self).__name__, self.id, self.x,
+            self.y, self.height
+        )
