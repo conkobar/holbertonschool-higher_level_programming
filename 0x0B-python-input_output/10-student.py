159758451ff7f2bf74b553134314d2640cec59a1
conkobar <connorkleine@gmail.com>
2022-09-19 22:07:37 -0500

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module for task 10"""
+
+
+class Student:
+    """defines student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        jays = vars(self)
+
+        if attrs:
+            jays = {}
+            for key, value in vars(self).items():
+                if key in attrs:
+                    jays[key] = value
+
+        return jays
