7f7bc344dcacd39c589799db499b47eda99df863
conkobar <connorkleine@gmail.com>
2022-09-23 09:51:47 -0500

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """module creating base class"""
 
 
+import json
+
+
 class Base:
     """base class from which all others in project will stem"""
     __nb_objects = 0
@@ -13,3 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns json string rep"""
+        rep = []
+        if list_dictionaries:
+            rep = json.dumps(list_dictionaries)
+        return rep
+
