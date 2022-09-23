fb87c453a232317a1614f595726c38c672fb833c
conkobar <connorkleine@gmail.com>
2022-09-23 10:12:54 -0500

    task 15 i am upset now

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,4 +22,5 @@ class Base:
         """returns json string rep"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return []
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
