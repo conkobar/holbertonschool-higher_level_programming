3c3be17a717ab1d41f48b58f6e38239ba9515a37
conkobar <connorkleine@gmail.com>
2022-09-23 10:19:58 -0500

    task 15 did i figure it out

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns json string rep"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
-        else:
-            return json.dumps(list_dictionaries)
+        return (
+            "[]" if list_dictionaries is None or list_dictionaries is []
+            else json.dumps(list_dictionaries)
+        )
