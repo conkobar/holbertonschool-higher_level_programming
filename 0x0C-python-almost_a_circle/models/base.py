fed27a3c92606ae9198e64082579ce6e7451a7b5
conkobar <connorkleine@gmail.com>
2022-09-23 15:13:54 -0500

    issue was i was returning a string and not an empty list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,5 +37,5 @@ class Base:
     def from_json_string(json_string):
         """returns list of json string"""
         return (
-            "[]" if json_string is None or 0 else json.loads(json_string)
+            [] if json_string is None or 0 else json.loads(json_string)
             )
