a615e1f4fd0d14b999bf4bfce65ec2c7522bb324
conkobar <connorkleine@gmail.com>
2022-10-06 20:26:27 -0500

    task 0, refining reqs

--- 0x01-SQL_more_queries/0-priveleges.sql
+++ 0x01-SQL_more_queries/0-priveleges.sql
@@ -1,3 +1,3 @@
--- lists all priveleges of local two users
+-- lists all priveleges of user_0d_1, user_0d_2 in localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
