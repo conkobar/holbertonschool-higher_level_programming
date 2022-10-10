fe98df44a0f43fdade47bb35f25a9d205cc5cf58
conkobar <connorkleine@gmail.com>
2022-10-10 10:50:09 -0500

    task 9 trying again

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in database hbtn_0d_usaSELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states ON cities.state_id = states.id;
+SELECT cities.state_id FROM cities INNER JOIN states ON cities.state_id = states.id;
