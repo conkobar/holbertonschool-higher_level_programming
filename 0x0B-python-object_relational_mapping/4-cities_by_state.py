cff00e61da06d027c8b0acbb1b75525685aa1027
conkobar <connorkleine@gmail.com>
2022-10-19 23:43:38 -0500

    task 4; pep8 and closed db

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON \
-            cities.state_id = states.id ORDER BY cities.id"
+        "SELECT cities.id, cities.name, states.name FROM cities JOIN states \
+            ON cities.state_id = states.id ORDER BY cities.id"
     )
     for city in cur.fetchall():
         print(city)
+    db.close()
+    cur.close()
