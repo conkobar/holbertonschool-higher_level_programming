400c497d8233087edf9daa348dacda81895c9dd2
conkobar <connorkleine@gmail.com>
2022-10-20 16:34:59 -0500

    task 6

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""contains definition and instance of a class"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class for sqlalchemy"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
