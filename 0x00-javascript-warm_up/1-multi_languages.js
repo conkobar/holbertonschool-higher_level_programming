105d7a69c863055c1ea391105e38802a67feee35
conkobar <connorkleine@gmail.com>
2022-12-01 13:38:06 -0600

    task 1 but this time i bettied

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myVar = 'JavaScript is amazing';
-const cWorthy = 'C is fun'
-const bigSnake = 'Python is cool'
+const cWorthy = 'C is fun';
+const bigSnake = 'Python is cool';
 console.log('', cWorthy, '\n', myVar, '\n', bigSnake);
