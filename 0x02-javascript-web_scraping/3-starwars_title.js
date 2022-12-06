a17d8b0e5d6e3652cf6b869e2ebffe129a9abd7a
conkobar <connorkleine@gmail.com>
2022-12-06 11:35:02 -0600

    task 3 semi

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 request(url, (err, response, body) => {
   if (err) throw err;
